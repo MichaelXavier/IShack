ad8fc948fdc902cfb8b057f7af228a5e20faf5ef
Michael Xavier <michael@michaelxavier.net>
2010-06-10 00:23:51 -0700

    Fall back to RSpec 1.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require 'rspec'
+require 'spec'
 require 'lib/ishack'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-Rspec.configure do |config|
+Spec::Runner.configure do |config|
   config.mock_with :rspec
 end
